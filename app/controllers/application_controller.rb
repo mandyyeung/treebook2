97384c26f576554f6b334410b3066be5b8aaea7c
Mandy Yeung <mandycodes@gmail.com>
2014-04-21 11:41:47 -0400

    Created relationships in tables and added a dropdown for name field

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :profile_name
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :email
+    devise_parameter_sanitizer.for(:sign_up) << :password
+    devise_parameter_sanitizer.for(:sign_up) << :password_confirmation
+  end
 end
